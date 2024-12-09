da8f063c41a69fec41b0dacc4b0992856083befd
Matt Goldsworthy <matt.j.goldsworthy@gmail.com>
2024-12-08 22:38:33 -0700

    add updates, update readme

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# react-movie-app
- 
+# React Movie App
+
+This is a React-based application that can search and display movies for you.
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Running the Application](#running-the-application)
+- [License](#license)
+
+---
+
+## Getting Started
+
+Follow these instructions to set up and run the project on your local machine.
+
+---
+
+## Prerequisites
+
+Ensure you have the following installed on your machine:
+
+- [Node.js](https://nodejs.org/) (v16 or newer recommended)
+- [npm](https://www.npmjs.com/) (comes with Node.js)
+
+You can verify the installation using:
+
+```bash
+node -v
+npm -v
+```
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone <repository-url>
+   ```
+
+2. Navigate to project directory:
+   ```bash
+   cd <project-folder>
+   ```
+3. Install dependencies:
+   ```bash
+   npm install
+   ```
+
+## Running the Application
+
+To start the development server:
+
+```bash
+npm run dev
+```
+
+The application should be accessible at:
+
+```html
+http://localhost:5173
+```
+
+## License
+
+This project is licensed under the MIT License.
--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import Search from "./components/Search";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
 import {apiUrl, config} from "./constants";
+import { Pagination } from "@mui/material";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
     const [state, setState] = useState({
         s: "sherlock",
+        totalPages: 1,
+        currentPage: 1,
+        totalResults: 0,
         results: [],
-        selected: {},
     });
+
+    const [dataPage, setDataPage] = useState(0);
+
 
     const searchInput = (e) => {
         let s = e.target.value;
@@ -19,6 +29,22 @@ function App() {
             return { ...prevState, s: s };
         });
     };
+    const onDataPageChange = (event, page) => {
+        if (state.currentPage >= 1) {
+            axios(apiUrl + '/search/movie?query=' + state.s + '&page=' + page, config).then(
+                ({ data }) => {
+                    setDataPage(page - 1);
+                    setState((prevState) => {
+                        return {
+                            ...prevState,
+                            results: data.results,
+                            currentPage: page,
+                        };
+                    });
+                }
+            );
+        }
+    }
 
     const search = (e) => {
         if (e.key === "Enter") {
@@ -29,6 +55,10 @@ function App() {
                         return {
                             ...prevState,
                             results: results,
+                            totalPages: data.total_pages,
+                            currentPage: data.page,
+                            nextPage: data.page === data.total_pages ? data.page : data.page+1,
+                            totalResults: data.total_results,
                         };
                     });
                 }
@@ -46,7 +76,21 @@ function App() {
                     searchInput={searchInput}
                     search={search}
                 />
-
+                <div className="container">
+                    {state.totalPages > 1 ? (
+                        <Pagination
+                            color="primary"
+                            count={state.totalPages}
+                            onChange={onDataPageChange}
+                            page={dataPage + 1}
+                            variant="outlined" 
+                            shape="rounded"
+                        />
+                    ) : (
+                        <div />
+                    )}
+                </div>
+                <br />
                 <div className="container">
                     {state.results.map((movie) => (
                        <MovieCard  
@@ -55,6 +99,29 @@ function App() {
                             state={state}
                         />
                     ))}
+                </div>
+
+                <div className="container">
+                    {state.totalPages > 1 ? (
+                        <Pagination
+                            color="primary"
+                            count={state.totalPages}
+                            onChange={onDataPageChange}
+                            page={dataPage + 1}
+                            variant="outlined" 
+                            shape="rounded"
+                        />
+               
+                    ) : (
+                        <div />
+                    )}
+                </div>
+                <div className="container">
+                    {state.totalResults > 0 ? (
+                        <div>Total Results: {state.totalResults}</div>
+                    ) : (
+                        <div />
+                    )}
                 </div>
             </main>
         </div>
